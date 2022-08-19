54f3ad0fb3796ad9647276dd7263129226e9fbf1
yeonjoo.lee <teddyjune@naver.com>
2022-08-19 14:05:28 +0900

    feat: viewModel 수정

--- lib/data/repository/exchange_rate_repository.dart
+++ lib/data/repository/exchange_rate_repository.dart
@@ -1,9 +1,24 @@
+import 'package:exchange_currency_app/data/data_source/local/country_api.dart';
 import 'package:exchange_currency_app/data/data_source/remote/exchange_rate_api.dart';
+import 'package:exchange_currency_app/data/model/country.dart';
+import 'package:exchange_currency_app/data/model/exchange_rate.dart';
 
 class ExchangeRateRepository {
-  final _api = ExchangeRateApi();
+  final _countryApi = CountryApi();
+  final _exchangeApi = ExchangeRateApi();
 
-  Future<Map<String, dynamic>> getConversionRates(String query) async {
-    return await _api.getConversionRates(query);
+  Future<List<ExchangeRate>> getExchangeRateList(String query) async {
+    final conversionRates = await _exchangeApi.getConversionRates(query);
+    final countries = await _countryApi.getNationalName();
+    return countries.map((Country country) {
+      final conversionRate = conversionRates[country.currencyCode];
+      //화면에 나오는 정보 순서로 만듦. (국기/나라이름/통화명/환율)
+      return ExchangeRate(
+        imageUrl: country.imageUrl,
+        countryName: country.country,
+        currency: country.currencyCode,
+        conversionRate: conversionRate,
+      );
+    }).toList();
   }
 }
--- lib/ui/exchange_view_model.dart
+++ lib/ui/exchange_view_model.dart
@@ -1,21 +1,22 @@
+import 'package:exchange_currency_app/data/model/exchange_rate.dart';
 import 'package:exchange_currency_app/data/repository/exchange_rate_repository.dart';
 import 'package:flutter/material.dart';
 
 class ExchangeViewModel extends ChangeNotifier {
   final _repository = ExchangeRateRepository();
-  Map<String, dynamic> conversionRates = {};
-  List<String> shownList = [];
-  List<dynamic> valueList = [];
+  List<ExchangeRate> _exchangeRates = [];
+
+  List<ExchangeRate> get exchangeRates => List.unmodifiable(_exchangeRates);
   bool isLoading = false;
 
   Future fetchConversionRates(String query) async {
     isLoading = true;
     notifyListeners();
 
-    conversionRates = await _repository.getConversionRates(query);
-    shownList = conversionRates.keys.toList();
-    valueList = conversionRates.values.toList();
-    isLoading = false;
-    notifyListeners();
+    void onSearch(String query) async {
+      _exchangeRates = await _repository.getExchangeRateList(query);
+      isLoading = false;
+      notifyListeners();
+    }
   }
 }
